888c011f0408687de2220c53beb11516484bc513
Guilherme Buys <guibuys55@gmail.com>
2026-03-24 16:46:56 -0300

    feat: creating the authentication pages and types

---
+++ src/app/signin/page.tsx
@@ -0,0 +1,51 @@
+"use client"
+
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import type { UserSignInInterface } from "@/types/interfaces/userInterfaces";
+import { redirect } from "next/navigation";
+import { useForm } from "react-hook-form";
+
+import Link from 'next/link'
+
+import { zodResolver } from '@hookform/resolvers/zod'
+import { userSignInSchema } from "@/types/schemas/userSchemas";
+import { Card } from "@/components/ui/card";
+
+export default function SignInPage() {
+  const {register, handleSubmit} = useForm<UserSignInInterface>({
+    resolver: zodResolver(userSignInSchema)
+  })
+
+  const submitFunc = async (data: UserSignInInterface) => {
+    try {
+      redirect("/hub");
+    } catch (err) {
+      console.error("Erro no login:", err);
+    }
+  };
+
+  return (
+    <div className="flex items-center justify-center bg-linear-to-br from-cyan-500 to-blue-500 min-h-screen w-full">
+      <Card className="bg-white p-4 rounded-lg space-y-4 border-gray-700 border shadow-lg shadow-gray-800 w-[80%] md:w-96">
+        <div className="flex flex-col gap-2">
+          <h1 className="text-gray-800 font-bold text-2xl text-center">
+            Entre na sua Conta
+          </h1>
+          <p className="text-gray-600 text-center">
+            Realize o login para responder aos testes
+          </p>
+        </div>
+        <Input {...register("email")} className="border border-gray-500 text-gray-800 placeholder:text-gray-500 placeholder:font-medium" type="text" placeholder="E-mail" />
+        <Input {...register("password")} className="border border-gray-500 text-gray-800 placeholder:text-gray-500 placeholder:font-medium" type="password" placeholder="Senha" />
+
+        <div className="flex flex-col gap-2">
+          <Button onClick={handleSubmit(submitFunc)} className="bg-blue-500 text-xl p-4 text-white font-bold hover:bg-blue-700 cursor-pointer w-full" type="submit">
+            Entrar
+          </Button>
+          <p className="text-gray-700 text-center">Não está cadastrado? <Link className="hover:underline text-gray-600 hover:text-blue-500" href={"/signup"}>Crie uma conta</Link></p>
+        </div>
+      </Card>
+    </div>
+  )
+}
---
+++ src/app/signup/page.tsx
@@ -0,0 +1,50 @@
+"use client"
+
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import type { UserSignUpInterface } from "@/types/interfaces/userInterfaces";
+import { redirect } from "next/navigation";
+import { useForm } from "react-hook-form";
+
+import { zodResolver } from '@hookform/resolvers/zod'
+import { userSignUpSchema } from "@/types/schemas/userSchemas";
+import { Card } from "@/components/ui/card";
+
+export default function SignUpPage() {
+  const {register, handleSubmit} = useForm<UserSignUpInterface>({
+    resolver: zodResolver(userSignUpSchema)
+  })
+
+  const submitFunc = async (data: UserSignUpInterface) => {
+    try {
+      redirect("/hub");
+    } catch (err) {
+      console.error("Erro no login:", err);
+    }
+  };
+
+  return (
+    <div className="flex items-center justify-center bg-linear-to-br from-cyan-500 to-blue-500 min-h-screen w-full">
+      <Card className="bg-white p-4 rounded-lg space-y-4 border-gray-700 border shadow-lg shadow-gray-800 w-[80%] md:w-95">
+        <div className="flex flex-col gap-2">
+          <h1 className="text-gray-800 font-bold text-2xl text-center">
+            Crie sua Conta
+          </h1>
+          <p className="text-gray-600 text-center">
+            Crie uma conta para responder aos testes
+          </p>
+        </div>
+        <Input {...register("name")} className="border border-gray-500 text-gray-800 placeholder:text-gray-500 placeholder:font-medium" type="text" placeholder="Nome de usuÃ¡rio" />
+        <Input {...register("email")} className="border border-gray-500 text-gray-800 placeholder:text-gray-500 placeholder:font-medium" type="text" placeholder="E-mail" />
+        <Input {...register("password")} className="border border-gray-500 text-gray-800 placeholder:text-gray-500 placeholder:font-medium" type="password" placeholder="Senha" />
+        <Input {...register("confirmPassword")} className="border border-gray-500 text-gray-800 placeholder:text-gray-500 placeholder:font-medium" type="password" placeholder="Confirmar senha" />
+
+        <div className="flex flex-col gap-2">
+          <Button onClick={handleSubmit(submitFunc)} className="bg-blue-500 text-xl p-4 text-white font-bold hover:bg-blue-700 cursor-pointer w-full" type="submit">
+            Cadastrar
+          </Button>
+        </div>
+      </Card>
+    </div>
+  )
+}
